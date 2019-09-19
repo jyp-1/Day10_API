040b070ea6c2d7744d995dbeed58164b759d7c5b
jyp-1 <supersonic11@daum.net>
2019-09-19 14:55:51 +0900

    Test

---
+++ src/com/jy/t1/Test.java
@@ -0,0 +1,18 @@
+package com.jy.t1;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		//스캐너를 사용..
+		//주민등록번호 입력 991222-1234567
+		//나이
+		//계절
+		//성별
+		
+		
+		
+		
+	}
+
+}
