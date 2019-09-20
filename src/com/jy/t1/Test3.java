5ba352a49934baab874944b209dfba7db7faa0d9
jyp-1 <supersonic11@daum.net>
2019-09-20 10:48:20 +0900

    Update Test3.java

--- src/com/jy/t1/Test3.java
+++ src/com/jy/t1/Test3.java
@@ -1,5 +1,31 @@
 package com.jy.t1;
 
 public class Test3 {
+	
+	public static void main(String[] args) {
+		double b = Math.ceil(3.22);  //올림
+		System.out.println(b);
+		
+		b=6.12;
+		b=Math.floor(b);     		//내림
+		System.out.println(b);
+		
+		b=6.48;
+		b=Math.round(b);			//반올림(소숫점 첫번째 자리 기준)
+		System.out.println(b);
+		
+		double d =Math.random();
+				
+		System.out.println((int)(10*d));
+		
+
+		
+		//0123456789 랜덤 숫자
+		
+		
+		
+		
+		
+	}
 
 }
