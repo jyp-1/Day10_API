f6b1ac38c8b076529050105aef6fcf5f59b1bc39
jyp-1 <supersonic11@daum.net>
2019-09-20 10:15:20 +0900

    jumin correct

--- src/com/jy/t1/Test2.java
+++ src/com/jy/t1/Test2.java
@@ -6,9 +6,9 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// 주민등록번호 991222-1234567
-		// 9  7  1  2  2  2  1  2  3  4  5  6 7(검증용 번호)
-	  // * 2  3  4  5  6  7  8  9  2  3  4  5
-	// 2. 18  21 4 10 12 14  8 18  6 12 20 30 = 173
+		// 9 7 1 2 2 2 1 2 3 4 5 6 7(검증용 번호)
+		// * 2 3 4 5 6 7 8 9 2 3 4 5
+		// 2. 18 21 4 10 12 14 8 18 6 12 20 30 = 173
 		// 3. 173/11 --몫 15 나머지 8
 
 		// 4. 11 - 8(나머지) --8(결과값)
@@ -17,29 +17,82 @@ public class Test2 {
 		// 5. 결과값 == 검증용 번호
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("주민등록번호를 입력하세요(-는 제외)");
+		System.out.println("주민등록번호를 입력하세요");
 		String jumin = sc.next();
 		
-		//주민번호 
-		int[] jg =new int [13];				//주민등록 나열
-		
-		int [] g = {2,3,4,5,6,7,8,9,2,3,4,5}; //곱할 것들
-		
-		int[] result = new int[13];				//곱한 결과
+		int sum=0; //합계를 담을 변수
+		int idx=2; //
 		
 
-		for (int i = 0; i < jumin.length(); i++) { 
-			String a = jumin.substring(i, i + 1);  //주민등록번호의 길이만큼 하나씩 분리
-			jg[i]=Integer.parseInt(a);			   //배열->숫자
+		// split 사용
+		// String[] jumins= jumin.split("");
+		// for(int i=0;i<jumin.length();i++) { System.out.println(jumins[i]); }
+		/**
+		 * for (int i = 0; i < jumin.length(); i++) { String n1 = jumin.substring(i, i +
+		 * 1); System.out.println(n1);
+		 * 
+		 * int n = Integer.parseInt(n1);
+		 * }
+		 * 내가 해본거 
+		 * 
+		 */
+		
+		
+		
+		
+		// charAt사용
+		for (int i = 0; i < jumin.length(); i++) {
+			if(i==6) {
+				continue;
+			}
+			char ch = jumin.charAt(i);
+			String n1 = String.valueOf(ch);
+			
+			int n = Integer.parseInt(n1);
+			
+			sum=sum+n*idx; //계산식
+			idx++;
+			
+			System.out.println(ch);
 
-			System.out.println("학원에서 작업");
+			if(idx==10) {
 
-			System.out.println("집에서 작업");
+				idx=2;
+
+			}
 
 		}
-		
+
 		
 
+		sum=sum%11;
+
+		
+
+		sum = 11-sum;
+
+		
+
+		sum = sum%10;
+
+		
+
+		
+
+		int result =  Integer.parseInt(String.valueOf(jumin.charAt(jumin.length()-1)));
+
+		
+
+		if(sum == result) {
+
+			System.out.println("맞는 번호");
+
+		}else {
+
+			System.out.println("틀린 번호");
+
+		}
+			
+
+		}
 	}
-
-}
---
+++ src/com/jy/t1/Test3.java
@@ -0,0 +1,5 @@
+package com.jy.t1;
+
+public class Test3 {
+
+}
